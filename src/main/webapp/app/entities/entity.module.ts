327a3ae07ffce2add035f40ec83a6af71ea1891a
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-02-28 17:01:14 -0500

    Initial application generated by JHipster-5.8.1

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,22 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
-            {
-                path: 'point',
-                loadChildren: './point/point.module#TwentyOnePointsPointModule'
-            },
-            {
-                path: 'weight',
-                loadChildren: './weight/weight.module#TwentyOnePointsWeightModule'
-            },
-            {
-                path: 'blood-pressure',
-                loadChildren: './blood-pressure/blood-pressure.module#TwentyOnePointsBloodPressureModule'
-            },
-            {
-                path: 'preference',
-                loadChildren: './preference/preference.module#TwentyOnePointsPreferenceModule'
-            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
