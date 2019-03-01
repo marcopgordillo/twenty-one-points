6123500a8ff07802df31f5ecead142380dea40a0
marcopgordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-03-01 10:26:27 -0500

    Updated Preference relationship one-to-one

--- src/main/java/org/jhipster/health/domain/Preference.java
+++ src/main/java/org/jhipster/health/domain/Preference.java
@@ -1,18 +1,17 @@
 package org.jhipster.health.domain;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.jhipster.health.domain.enumeration.Unit;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
-
-import org.springframework.data.elasticsearch.annotations.Document;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Objects;
-
-import org.jhipster.health.domain.enumeration.Unit;
 
 /**
  * A Preference.
@@ -41,7 +40,7 @@ public class Preference implements Serializable {
     private Unit weightUnits;
 
     @OneToOne
-    @JsonIgnoreProperties("preferences")
+    @JoinColumn(unique = true)
     private User user;
 
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
